09a65a502132fcc74f3e968a9713a8645c6ce3f0
Farai Madzima <farai@me.com>
2016-01-08 16:40:09 +0200

    Added workshops model

---
+++ models/Workshop.js
@@ -0,0 +1,35 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Post Model
+ * ==========
+ */
+
+var Workshop = new keystone.List('Workshop', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true }
+});
+
+Workshop.add({
+	title: { type: String, required: true },
+	level: { type: Types.Select, options: 'beginner, advanced', default: 'beginner', index: true },
+	eventDate: { type: Types.Date, index: true },
+	content: {
+		brief: { type: Types.Markdown, height: 150 },
+		extended: { type: Types.Markdown, height: 400 },
+		prerequisites: { type: Types.Markdown, height: 180 },
+		audience: { type: Types.Markdown, height: 180 },
+		takeaways: { type: Types.Markdown, height: 180 },
+		morning: { type: Types.Markdown, height: 180 },
+		afternoon: { type: Types.Markdown, height: 180 }
+	},
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+});
+
+Workshop.schema.virtual('content.full').get(function() {
+	return this.content.extended || this.content.brief;
+});
+
+Workshop.defaultColumns = 'title, eventDate|20%';
+Workshop.register();
