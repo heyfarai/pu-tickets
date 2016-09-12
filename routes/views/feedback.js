64b888a48343270602fea44979d370a8a462eb1f
mr-farai <faraimadzima@farai-2.local>
2016-09-12 10:34:33 +0200

    Adde scribbles email

--- routes/views/feedback.js
+++ routes/views/feedback.js
@@ -2,6 +2,6 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-	res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSd6NrAbXd_GhM0bySyGMeRCf2gzCrbWSbPT4oaDzyM3n1QGZw/viewform')
+	res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSd6NrAbXd_GhM0bySyGMeRCf2gzCrbWSbPT4oaDzyM3n1QGZw/viewform====')
 
 };
