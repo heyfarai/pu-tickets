240281eae998afa48b08c59b36c2d293ef9600be
mr-farai <faraimadzima@farai-2.local>
2016-12-18 15:36:29 +0200

    added worker

---
+++ worker.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var keystone = require('keystone');
+
+var url = (keystone.get('env')=="development") ? "http://localhost:8888/api/" : "https://register.pixelup.co.za/api/" ;
+
+request({
+    url: url, //URL to hit
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+}, function (error, response, body) {
+    if(error) {
+        console.log(error);
+    } else {
+         console.log(response.statusCode, body);
+    }
+});
